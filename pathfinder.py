eda50e9fda77eb61b6b87a321b914ed05e287906
Ardra Salemy <ardrasalemy@gmail.com>
2018-11-12 12:43:46 -0500

    Trouble with variable 'rgb_number'

--- pathfinder.py
+++ pathfinder.py
@@ -1,6 +1,11 @@
+from PIL import Image
+
+
 def open_file(file):
     """
-    Opens file, theoretically 'elevation_small_file' should display formatted version--not sure why that didn't display. 'elevation' displayed list of strings of numbers with '/n'.
+    Opens file, theoretically 'elevation_small_file' should display formatted
+    version--not sure why that didn't display. 'elevation', if printed, lists
+    strings of numbers with '/n'.
     """
     elevation_small_file = open(file, 'r')
     elevation = elevation_small_file.readlines()
@@ -8,55 +13,66 @@ def open_file(file):
 
 
 def clean_elevation(elevation):
-    breakpoint()
+    
     """
-    'Clean_elevation' strips '\n' and separates each number into it's own string. 'Elevation_list' turns the strings into integers.
+    'Clean_elevation' strips '\n' and separates each number into it's own
+    string. 'Elevation_list' turns the strings into integers.
     """
     cleaned_elevation_list = [letter.replace(
         '\n', '').strip().split() for letter in open_file(file)]
-    elevation_list = [[int(cleaned_elevation_list) for cleaned_elevation_list in row] for row in cleaned_elevation_list]
+    elevation_list = [[int(cleaned_elevation_list) for cleaned_elevation_list
+                      in row] for row in cleaned_elevation_list]
     return elevation_list
 
 
 def find_max(elevation_list):
     """
-    Gets the maximum elevation in each row, and then returns the maximum for all rows. *I'm not sure why this one stopped working when I moved it into a function. Before it printed all the numbers, and then printed 5561 at the end, so I will use that as the max going forward.
+    Gets the maximum elevation in each row, and then returns the maximum for
+    all rows--I suspect this isn't working how I intended, but it's close!
     """
-    max_elevation= max(elevation_list)
-    print(max_elevation)
-    max_max_elevation= max(max_elevation)
-    # print(max_max_elevation)
-    return max_max_elevation
+    maximum_elevation_per_row = max(elevation_list)
+    maximum_point = max(maximum_elevation_per_row)
+    return maximum_point
 
 
 def find_min(elevation_list):
     """
-    Same as max, but for minimum elevation. *This one also stopped working when I moved it into a function. Previously I got 3143 as the min.
+    Same as max, but for minimum elevation.
     """
-    min_elevation= min(elevation_list)
-    min_min_elevation= min(min_elevation)
-    # print(min_min_elevation)
-    # return min_min_elevation
+    minimum_elevation_per_row = min(elevation_list)
+    minimum_point = min(minimum_elevation_per_row)
+    return minimum_point
 
 
-# def scale_elevation():
-#     """
-#     Scale the minimum to maximum range to a 0-255 range, in order to convert elevations to a greyscale image, using the formula: (elevation/max elevation) * 255
-#     """
-#     for number in elevation_list:
-#         new_int = int((number/5561) * 255)
-#         print(new_int)
-#         return new_int
+def scale_elevation(number):
+    """
+    Scale the minimum to maximum range to a 0-255 range, in order to convert
+    elevations to a greyscale image.
+    """
+    maximum_point = 5561
+    minimum_point = 3143
+    rgb_number = [[round(int((y) - minimum_point)/(maximum_point -
+                   minimum_point) * 255) for y in x] for x in number]
+    return rgb_number
+   
+
+def plot_picture(list):
+    """
+    Takes the scaled 'rgb_number'(s) and puts them in x,y format to plot
+    picture in greyscale.
+    """
+    # for index, num in enumerate(row):
+    #     print(index)
+    for y, row in enumerate(rgb_number):
+        for x, num in enumerate(row):
+            img.putpixel((x,y), (num, num, num))
+            img.save('test.png')
 
 
-
-
-
-elevation_list= list
-file= 'elevation_small.txt'
-elevation= open_file(file)
-clean_elevation= clean_elevation(elevation)
-
-max= find_max(elevation_list)
-# min = min(elevation_list)
-# scale_elevation()
+file = 'elevation_small.txt'
+elevation = open_file(file)
+elevation_list = clean_elevation(elevation)
+max = find_max(elevation_list)
+min = find_min(elevation_list)
+scale_elevation(elevation_list)
+plot_picture(rgb_number)
